db9b9773443831e3a017a93c9c85a497d8e160bf
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2022-12-17 12:12:32 +0100

    AB#82 added service plan

--- main.tf
+++ main.tf
@@ -1,6 +1,28 @@
 
 #Create ressource group for iaac-azure-tf-wapp
 resource   "azurerm_resource_group"   "rg"   { 
-  name                =   var.rg_name 
-  location            =   var.rg_location 
+  name                =   var.wap_rg_name 
+  location            =   var.wap_rg_location
+
+  tags = {
+    owner       = "fred"
+    dept        = "IT"
+  }
+}
+
+resource "azurerm_app_service_plan" "wap_sp" {
+  name                = var.wap_sap_name
+  location            = var.wap_rg_location
+  resource_group_name = var.wap_rg_name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  tags = {
+    owner       = "fred"
+    dept        = "IT"
+  }
+
 }
