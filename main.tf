86cf13b92f6f4d83cca9976d348964178914e6c5
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2022-12-17 13:33:03 +0100

    AB#83 added qa and prod azurerm_linux_web_app

--- main.tf
+++ main.tf
@@ -35,6 +35,39 @@ resource "azurerm_linux_web_app" "wap_dv_name" {
   tags = {
     owner       = var.owner
     dept        = var.department
+    status      = var.wap_status_dv
+  }
+
+  site_config {}
+}
+
+resource "azurerm_linux_web_app" "wap_qa_name" {
+  name                = var.wap_qa_name
+  resource_group_name = var.wap_rg_name
+  location            = var.wap_rg_location
+  service_plan_id     = azurerm_service_plan.wap_sp_name.id
+  depends_on          = [azurerm_resource_group.rg, azurerm_service_plan.wap_sp_name]
+
+  tags = {
+    owner       = var.owner
+    dept        = var.department
+    status      = var.wap_status_qa
+  }
+
+  site_config {}
+}
+
+resource "azurerm_linux_web_app" "wap_pd_name" {
+  name                = var.wap_pd_name
+  resource_group_name = var.wap_rg_name
+  location            = var.wap_rg_location
+  service_plan_id     = azurerm_service_plan.wap_sp_name.id
+  depends_on          = [azurerm_resource_group.rg, azurerm_service_plan.wap_sp_name]
+
+  tags = {
+    owner       = var.owner
+    dept        = var.department
+    status      = var.wap_status_pd
   }
 
   site_config {}
