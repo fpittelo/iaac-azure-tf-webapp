d432467b5ace2410bffa3983513688a2b283f51a
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2022-12-17 13:25:45 +0100

    AB#83 created azurerm_linux_web_app

--- main.tf
+++ main.tf
@@ -10,16 +10,32 @@ resource   "azurerm_resource_group"   "rg"   {
   }
 }
 
-resource "azurerm_service_plan" "wap_sp" {
-  name                = var.wap_sap_name
+resource "azurerm_service_plan" "wap_sp_name" {
+  name                = var.wap_sp_name
   location            = var.wap_rg_location
   resource_group_name = var.wap_rg_name
-  sku_name            = var.wap_sp_sku_name_P1v2
+  sku_name            = var.wap_sp_sku_name_F1
   os_type             = var.wap_sp_sku_os_linux
+  depends_on          = [azurerm_resource_group.rg]
 
   tags = {
     owner       = var.owner
     dept        = var.department
   }
 
+}
+
+resource "azurerm_linux_web_app" "wap_dv_name" {
+  name                = var.wap_dv_name
+  resource_group_name = var.wap_rg_name
+  location            = var.wap_rg_location
+  service_plan_id     = azurerm_service_plan.wap_sp_name.id
+  depends_on          = [azurerm_resource_group.rg, azurerm_service_plan.wap_sp_name]
+
+  tags = {
+    owner       = var.owner
+    dept        = var.department
+  }
+
+  site_config {}
 }
