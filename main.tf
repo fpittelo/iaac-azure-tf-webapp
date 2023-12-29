9d44a997c4b80e6c3ef817b1cb0c30d5845cc1c8
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2023-12-29 11:23:16 +0100

    AB#123 last update with "depends on"

--- main.tf
+++ main.tf
@@ -59,12 +59,6 @@ resource "azurerm_linux_web_app_slot" "dev" {
   }
 }
 
-resource "azurerm_app_service_source_control_slot" "wap_dv_source_control" {
-  slot_id             = azurerm_linux_web_app_slot.dev.id
-  repo_url            = "https://github.com/fpittelo/website"
-  branch              = "dev"
-}
-
 resource "azurerm_linux_web_app_slot" "qa" {
   name            = "qa"
   app_service_id  = azurerm_linux_web_app.wap_dv_name.id
@@ -89,13 +83,12 @@ resource "azurerm_linux_web_app_slot" "prod" {
   }
 }
 
-
-#resource "azurerm_app_service_source_control_slot" "wap_dv_source_control" {
-# slot_id             = azurerm_linux_web_app_slot.dev.id
-# repo_url            = "https://github.com/fpittelo/website"
-# branch              = "dev"
-#}
-
+resource "azurerm_app_service_source_control_slot" "wap_dv_source_control" {
+  slot_id             = azurerm_linux_web_app_slot.dev.id
+  repo_url            = "https://github.com/fpittelo/website"
+  branch              = "dev"
+  depends_on          = [ azurerm_linux_web_app.wap_dv_name ]
+}
 
 #resource "azurerm_linux_web_app" "wap_qa_name" {
 # name                = var.wap_qa_name
