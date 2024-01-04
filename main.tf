cce38ea0ab25f3733d94df483eb12d8008deb1a6
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-01-04 13:16:09 +0100

    AB#132 updated ip rules

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_key_vault" "iaacvault" {
   network_acls {
     bypass          = "AzureServices"
     default_action  = "Deny"
-    ip_rules        = ["178.193.30.41", "108.140.5.168/30"]
+  # ip_rules        = ["178.193.30.41", "108.140.5.168/30", "4.233.0.0/16"]
   }
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
