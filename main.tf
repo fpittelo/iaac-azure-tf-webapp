5484876a42e1705dda74dfc70fa0f325d268a96d
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2024-01-05 14:19:47 +0100

    AB#132 update acces policy

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@
 # }
 #}
 
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "iaacvault" {
   name                            = var.vault_name
   location                        = var.wap_rg_location
   resource_group_name             = var.wap_rg_name
+  tenant_id                       = var.tenant_id
+  sku_name                        = "standard"
   enabled_for_deployment          = true
   enabled_for_disk_encryption     = true
   enabled_for_template_deployment = true
-  tenant_id                       = data.azurerm_client_config.current.tenant_id
-  sku_name                        = "standard"
 # soft_delete_retention_days      = 7
   purge_protection_enabled        = false
   public_network_access_enabled   = true
@@ -32,44 +32,37 @@ resource "azurerm_key_vault" "iaacvault" {
   }
   
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+  tenant_id = var.tenant_id
+  object_id = var.sp_object_id
+  # tenant_id = data.azurerm_client_config.current.tenant_id
+  # object_id = data.azurerm_client_config.current.object_id
     
     certificate_permissions = [
+      "Get",
+      "List",
       "Create",
       "Update",
       "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
       "Purge",
-      "Get",
-      "List",
     ]
 
     key_permissions = [
+      "Get",
+      "List",
       "Create",
       "Update",
       "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
       "Purge",
-      "Get",
-      "List",
     ]
+    
     secret_permissions = [
-      "Get",
-    ]
-    storage_permissions = [
-      "Set",
       "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
       "Purge",
-      "Get",
-      "List",
+    ]
+    
+    storage_permissions = [
+      "Delete",
+      "Purge",
     ]
   }
 }
